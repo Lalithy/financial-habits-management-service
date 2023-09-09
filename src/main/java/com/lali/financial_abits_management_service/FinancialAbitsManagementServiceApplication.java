fb3d0f3c820ecc0c47546aa898d578119f91f991
Lali <lalithyanawanjana@gmail.com>
2023-09-09 19:37:35 +0530

    Initial commit

---
+++ src/main/java/com/lali/financial_abits_management_service/FinancialAbitsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lali.financial_abits_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAbitsManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAbitsManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lali/financial_abits_management_service/FinancialAbitsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lali.financial_abits_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAbitsManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
